517ddf590de14124c803cc3505a428cc2dda0c2c
Viviann09 <viviantan0910@gmail.com>
2024-09-19 21:47:13 +0700

    membuat screen my order, order summary, checkout detail

--- meal_go/lib/main.dart
+++ meal_go/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:meal_go/home.dart';
 import 'package:meal_go/restaurant.dart';
 import 'package:meal_go/search_food.dart';
 import 'package:meal_go/cart.dart';
+import 'package:meal_go/checkout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Meal Go',
-        debugShowCheckedModeBanner: false,
-        home: const HomePage(),
-        routes: {
-          '/backtohomepage': (context) => const HomePage(),
-          '/searchfood': (context) => const SearchFood(),
-          '/cartpage': (context) => CartPage(),
-        });
+      title: 'Meal Go',
+      debugShowCheckedModeBanner: false,
+      home: CheckoutPage(),
+      // home: const HomePage(),
+      // routes: {
+      //   '/backtohomepage': (context) => const HomePage(),
+      //   '/searchfood': (context) => const SearchFood(),
+      //   '/cartpage': (context) => CartPage(),
+      // }
+    );
   }
 }
