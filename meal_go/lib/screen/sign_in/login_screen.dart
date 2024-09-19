827f985364bfd2b19c4a8d7513d0310aa5327f14
MakotoMachiYuki <joshua1412a.o.t@gmail.com>
2024-09-19 21:48:44 +0700

    Validation

--- meal_go/lib/screen/sign_in/login_screen.dart
+++ meal_go/lib/screen/sign_in/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:meal_go/restaurant.dart';
 import 'package:meal_go/screen/sign_in/sign_in_screen.dart';
 
@@ -10,100 +11,148 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  var _passwordVisible = false;
+
+  void login() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? password = prefs.getString(_email.text);
+
+    if (password != null && password == _password.text) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const StoreDetail()));
+      }
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Invalid email or password!"),
+            backgroundColor: Colors.red));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: const Text('Log In'),
-        backgroundColor: const Color.fromRGBO(255, 204, 153, 1),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SignIn()),
-            );
-          },
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        return true;
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          title: const Text('Log In'),
+          backgroundColor: const Color.fromRGBO(255, 204, 153, 1),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+            onPressed: () {
+              ScaffoldMessenger.of(context).clearSnackBars();
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SignIn()),
+              );
+            },
+          ),
         ),
-      ),
-      body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Container(
-            color: Colors.orange,
-            child: SingleChildScrollView(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Log In to Go Meal!',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  const TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      labelStyle: TextStyle(color: Colors.black),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: TextStyle(color: Colors.black),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 30.0),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.redAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
+        body: SafeArea(
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Container(
+              color: Colors.orange,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30.0, vertical: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Log In to Go Meal!',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       ),
-                      padding: const EdgeInsets.symmetric(vertical: 15),
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const StoreDetail(),
+                    const SizedBox(height: 20.0),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _email,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.email_outlined),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Colors.black),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromRGBO(98, 0, 238, 1))),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: !_passwordVisible,
+                      controller: _password,
+                      decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.password_outlined),
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(color: Colors.black),
+                          filled: true,
+                          fillColor: Colors.white,
+                          enabledBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromRGBO(98, 0, 238, 1))),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                          )),
+                    ),
+                    const SizedBox(height: 30.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.redAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
                         ),
-                      );
-                    },
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.login, color: Colors.white),
-                        SizedBox(width: 10),
-                        Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontStyle: FontStyle.italic,
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      onPressed: () {
+                        login();
+                      },
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.login, color: Colors.white),
+                          SizedBox(width: 10),
+                          Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                ],
+                    const SizedBox(height: 5),
+                  ],
+                ),
               ),
             ),
           ),
--- meal_go/lib/screen/sign_in/sign_up_screen.dart
+++ meal_go/lib/screen/sign_in/sign_up_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:meal_go/screen/sign_in/login_screen.dart';
 import 'package:meal_go/screen/sign_in/sign_in_screen.dart';
 
@@ -10,14 +12,60 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final _formkey = GlobalKey<FormState>();
   final FocusNode _nameFocusNode = FocusNode();
   final FocusNode _emailFocusNode = FocusNode();
+
+  final nameValidator = ValidationBuilder()
+      .minLength(3, 'Name must be at least 3 characters')
+      .maxLength(30, 'name must not be over 30 characters')
+      .regExp(RegExp(r'^[a-zA-Z]+$'), 'Name only contain letter')
+      .build();
+
+  final emailValidator =
+      ValidationBuilder().email('Input a valid email address').build();
+
+  final passwordValidator = ValidationBuilder()
+      .minLength(8, 'Password must be at least 8 characters')
+      .regExp(RegExp(r'(?=.*[0-9])'), 'Password must contain a number')
+      .regExp(RegExp(r'(?=.*[a-z])'), 'Password must contain a lowercase')
+      .regExp(RegExp(r'(?=.*[A-Z])'), 'Password must contain an uppercase')
+      .regExp(
+          RegExp(r'(?=.*[*\W])'), 'Password must contain a special character')
+      .build();
+  final phoneValidator =
+      ValidationBuilder().phone('Input a valid phone number').build();
+
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _phone = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _confirmPassword = TextEditingController();
 
   @override
   void dispose() {
     _nameFocusNode.dispose();
     _emailFocusNode.dispose();
     super.dispose();
+  }
+
+  var _passwordVisible = false;
+  var _confirmPasswordVisible = false;
+
+  void submitForm() async {
+    if (_formkey.currentState!.validate()) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      String email = _email.text;
+      String password = _password.text;
+
+      await prefs.setString(email, password);
+
+      if (mounted) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Login()));
+      }
+    }
   }
 
   @override
@@ -36,96 +84,178 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
       body: SafeArea(
-        child: Container(
-          color: Colors.orange,
-          child: SingleChildScrollView(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Create your Account',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                TextField(
-                  focusNode: _nameFocusNode,
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                const TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Confirm Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 30.0),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(255, 153, 102, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Container(
+            color: Colors.orange,
+            child: SingleChildScrollView(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Create your Account',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 15),
                   ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const Login()));
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.person_add, color: Colors.white),
-                      SizedBox(width: 10),
-                      Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontStyle: FontStyle.italic),
-                      ),
-                    ],
+                  const SizedBox(height: 20.0),
+                  Form(
+                    key: _formkey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          focusNode: _nameFocusNode,
+                          validator: nameValidator,
+                          controller: _name,
+                          decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.account_box_outlined),
+                              labelText: 'Name',
+                              labelStyle: TextStyle(color: Colors.black),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Color.fromRGBO(98, 0, 238, 1)))),
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _email,
+                          validator: emailValidator,
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.email_outlined),
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.black),
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(98, 0, 238, 1))),
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          keyboardType: TextInputType.phone,
+                          validator: phoneValidator,
+                          controller: _phone,
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.phone_android_outlined),
+                            labelText: 'Phone Number',
+                            labelStyle: TextStyle(color: Colors.black),
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(98, 0, 238, 1))),
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          obscureText: !_passwordVisible,
+                          controller: _password,
+                          validator: passwordValidator,
+                          decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.password_outlined),
+                              labelText: 'Password',
+                              labelStyle: const TextStyle(color: Colors.black),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: const UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Color.fromRGBO(98, 0, 238, 1))),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Colors.grey,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _passwordVisible = !_passwordVisible;
+                                  });
+                                },
+                              )),
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          obscureText: !_confirmPasswordVisible,
+                          controller: _confirmPassword,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return "The field is required";
+                            }
+
+                            if (val != _password.text) {
+                              return "The password didn't match";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.password_outlined),
+                            labelText: 'Confirm Password',
+                            labelStyle: const TextStyle(color: Colors.black),
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(98, 0, 238, 1))),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _confirmPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.grey,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _confirmPasswordVisible =
+                                      !_confirmPasswordVisible;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30.0),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromRGBO(255, 153, 102, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                          ),
+                          onPressed: () {
+                            submitForm();
+                          },
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.person_add, color: Colors.white),
+                              SizedBox(width: 10),
+                              Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontStyle: FontStyle.italic),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 25),
-              ],
+                ],
+              ),
             ),
           ),
         ),
