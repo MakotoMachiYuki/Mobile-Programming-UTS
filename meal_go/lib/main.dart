13575cb2b3207e4be0d09ecd8f91678e1a9eece2
Chiichii <sachigranita26@gmail.com>
2024-09-22 10:36:37 +0700

    struktur address dan profile

--- meal_go/lib/main.dart
+++ meal_go/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:meal_go/restaurant.dart';
 import 'package:meal_go/settings.dart';
+=======
+import 'package:meal_go/users_address.dart';
+>>>>>>> Stashed changes
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +19,23 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Meal Go',
       debugShowCheckedModeBanner: false,
+<<<<<<< Updated upstream
       home: Settings(),
+=======
+
+      //home: OrderTrack(),
+      // home: SignIn(),
+      // home: UsersProfile(),
+      home: UsersAddress(),
+      //home: CheckoutPage(),
+      //home: const HomePage(),
+      //home: RestaurantHome(),
+      // routes: {
+      //   '/backtohomepage': (context) => const HomePage(),
+      //   '/searchfood': (context) => const SearchFood(),
+      //   '/cartpage': (context) => CartPage(),
+      // }
+>>>>>>> Stashed changes
     );
   }
 }
