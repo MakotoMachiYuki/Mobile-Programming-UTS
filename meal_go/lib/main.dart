eb22eaae73156911e5a2d0f67ab2e9465a2a7ddd
Viviann09 <viviantan0910@gmail.com>
2024-09-29 16:18:22 +0700

    menambahkan efek icon favorit pada bagian popular dan mengganti gambar dan deskripsi menu

--- meal_go/lib/main.dart
+++ meal_go/lib/main.dart
@@ -9,7 +9,6 @@ import 'package:meal_go/cart.dart';
 import 'package:meal_go/checkout.dart';
 import 'package:meal_go/restaurant_home.dart';
 import 'package:meal_go/settings.dart';
-import 'package:meal_go/orderplaced.dart';
 // import 'restaurant_menu.dart';
 import 'model/cart.dart';
 import 'model/menuCatalog.dart';
@@ -36,27 +35,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Meal Go',
-        debugShowCheckedModeBanner: false,
-        // home: CheckoutPage(),
-        // routes: {
-        //   '/orderplaced': (context) => const OrderPlacedPage(),
-        // }
-        home: const HomePage(),
-        routes: {
-          '/backtohomepage': (context) => const HomePage(),
-          '/searchfood': (context) => const SearchFood(),
-          '/cartpage': (context) => CartPage(),
-        }
-
-        // home: RestaurantHome(),
-        );
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       title: 'Meal Go',
       debugShowCheckedModeBanner: false,
 
       //home: OrderTrack(),
-      // home: SignIn(),
+      //home: SignIn(),
       //home: CheckoutPage(),
       //home: const HomePage(),
       //home: RestaurantHome(),
