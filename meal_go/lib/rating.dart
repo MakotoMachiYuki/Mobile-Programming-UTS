1d6b5048c6eeaf39bb067a3ac0b5d0427c6eae8d
IkanDongo <noel.535230067@stu.untar.ac.id>
2024-10-04 15:23:48 +0700

    perubahan format agar kompak dengan screen yang lainnya

--- meal_go/lib/rating.dart
+++ meal_go/lib/rating.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_go/home.dart';
+import 'package:flutter/cupertino.dart';
 
 class Rating extends StatefulWidget {
   const Rating({super.key});
@@ -15,19 +16,23 @@ class _RatingState extends State<Rating> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange.shade100,
+        elevation: 0,
         leading: IconButton(
-          padding: EdgeInsets.all(10),
-          onPressed: () {},
-          iconSize: 15,
-          icon: Icon(Icons.arrow_back_ios_new),
+          icon: const Icon(CupertinoIcons.back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        title: Text(
-          'Go Meal',
-          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        title: const Text(
+          "Go Meal",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: true,
-        toolbarHeight: 40,
-        backgroundColor: const Color.fromARGB(255, 253, 224, 171),
       ),
       body: Center(
         child: Column(
@@ -58,15 +63,24 @@ class _RatingState extends State<Rating> {
                 );
               }),
             ),
-            SizedBox(height: 20),
-            TextButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => HomePage()),
                 );
               },
-              child: Text('Submit'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 255, 166, 0),
+              ),
+              child: const Text(
+                'Submit',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 53, 53, 53),
+                ),
+              ),
             ),
           ],
         ),
